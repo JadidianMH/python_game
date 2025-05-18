07b8b21a7dad94d475964df204d3bb727facce2b
Mhosein <mhmtsze@gmail.com>
2025-05-18 07:04:48 +0330

    added basic variables to loader.py

---
+++ core/loader.py
@@ -0,0 +1,8 @@
+import pygame
+
+# common variables
+scoreFont = pygame.font.SysFont('calibri', 20)
+titleFont = pygame.font.SysFont('calibri', 30)
+black = ( 30, 30, 30)
+white = (220,220,220)
+red =   (220, 30, 30)
