90c0056014f4e8c13f6d08d8401b5bf7ffae5e14
Mhosein <mhmtsze@gmail.com>
2025-05-18 15:34:30 +0330

    successfully added apple for our SNAKE!

--- MainLoop.py
+++ MainLoop.py
@@ -1,6 +1,7 @@
 import pygame
 import core.loader as loader
 import core.function as function
+import random
 
 pygame.init()
 
@@ -19,6 +20,12 @@ snakeSize = 20
 snakeSpeed = 20
 snakeVelocity = [snakeSpeed, 0]
 
+# Apples for the snake
+appleSize = snakeSize
+apple = {
+    "pos": [0, 0],
+    "isAvailable": False
+}
 
 snake.append([size[0] // 2, size[1] // 2])
 
@@ -31,25 +38,42 @@ while running:
             if event.key == pygame.K_UP:
                 snakeVelocity[1] = -snakeSpeed
                 snakeVelocity[0] = 0
-                print("UP")
+
             elif event.key == pygame.K_DOWN:
                 snakeVelocity[1] = snakeSpeed
                 snakeVelocity[0] = 0
-                print("DOWN")
+
             elif event.key == pygame.K_LEFT:
                 snakeVelocity[0] = -snakeSpeed
                 snakeVelocity[1] = 0
-                print("LEFT")
+
             elif event.key == pygame.K_RIGHT:
                 snakeVelocity[0] = snakeSpeed
                 snakeVelocity[1] = 0
-                print("RIGHT")
-        
-    snake.append([snake[0][0] + snakeVelocity[0], snake[0][1] + snakeVelocity[1]])
-    snake.pop(0)
+    print(len(snake))
+    
+    ate_apple = False
+
+    if not apple["isAvailable"]:
+        randomPos = [random.randint(0, size[0] // appleSize - 1) * appleSize, random.randint(0, size[1] // appleSize - 1) * appleSize]
+        apple["pos"] = randomPos
+        apple["isAvailable"] = True
+        function.draw_snake(appleSize, [apple["pos"]], screen, loader.red)
+    else:
+        if apple["pos"] == snake[-1]:
+            apple["isAvailable"] = False
+            ate_apple = True  # Snake will grow
+        function.draw_snake(appleSize, [apple["pos"]], screen, loader.red)
+
+    # Move snake
+    new_head = [snake[-1][0] + snakeVelocity[0], snake[-1][1] + snakeVelocity[1]]
+    snake.append(new_head)
+    if not ate_apple:
+        snake.pop(0)  # Only remove tail if not eating apple
+
     function.draw_snake(snakeSize, snake, pygame.display.get_surface(), loader.white)
 
     pygame.display.update()
-    clock.tick(10)
+    clock.tick(12)
     
 pygame.quit()
--- core/function.py
+++ core/function.py
@@ -1,7 +1,7 @@
 import pygame
 
-def show_score(score, screen, font):
-    scoreText = font.render('score:' + str(score), True, loader.black)
+def show_score(score, screen, font, color):
+    scoreText = font.render('score:' + str(score), True, color)
     screen.blit(scoreText, [35, 10])
 
 def draw_snake(snakeSize, snakeList, screen, color):
@@ -12,3 +12,6 @@ def message(status, size, screen, color, bg, font):
     pygame.draw.rect(screen, bg, [0, 0, size[0], size[1]])
     Message = font.render(status, True, color)
     screen.blit(Message, size / 2)
+
+def draw_apple(appleSize, apple, screen, color):
+    pygame.draw.rect(screen, color, [apple["pos"][0], apple["pos"][1], appleSize, appleSize])
