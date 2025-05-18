c4b7422783d564d41867b55ae1be698d603737db
Mhosein <mhmtsze@gmail.com>
2025-05-18 15:56:56 +0330

    Fonts are ready

--- core/loader.py
+++ core/loader.py
@@ -4,8 +4,8 @@ pygame.init()
 pygame.font.init()
 
 # common variables
-scoreFont = pygame.font.SysFont('calibri', 20)
-titleFont = pygame.font.SysFont('calibri', 30)
+scoreFont = pygame.font.SysFont('Home Video', 20)
+titleFont = pygame.font.SysFont('Home Video', 30)
 black = ( 30, 30, 30)
 white = (220,220,220)
 red =   (220, 30, 30)
