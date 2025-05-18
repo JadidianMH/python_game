90e32e944949e84c4d15c56da63fba26acb2e7d2
Mhosein <mhmtsze@gmail.com>
2025-05-18 07:06:03 +0330

    working on MainLoop.py by adding some varaibles

--- MainLoop.py
+++ MainLoop.py
@@ -7,7 +7,13 @@ size = [600,800]
 pygame.display.set_mode(size)
 pygame.display.set_caption("Snake Game")
 
+# neccessary variables
+clock = pygame.time.Clock()
 running = True
+
+# Snake properties
+snakeSize = 20
+snakeSpeed = 20
 
 while running:
     for event in pygame.event.get():
